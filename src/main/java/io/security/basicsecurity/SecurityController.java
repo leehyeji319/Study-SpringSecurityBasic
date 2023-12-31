065a8c5b5c27737aa5b634ed33111d39ece5403b
zzambbang <zzambbang@gmail.com>
2023-12-31 19:21:30 +0900

    git init

---
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -0,0 +1,24 @@
+package io.security.basicsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *packageName    : io.security.basicsecurity
+ * fileName       : SecurityController
+ * author         : modsiw
+ * date           : 2023/12/31
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023/12/31        modsiw       최초 생성
+ */
+@RestController
+public class SecurityController {
+
+	@GetMapping("/")
+	public String index() {
+		return "home";
+	}
+}
